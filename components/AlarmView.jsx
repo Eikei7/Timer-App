c7883f5074bca640487295a36bdb75def2bc32aa
Erik Karlsson <erikmatfors@gmail.com>
2024-10-24 10:55:20 +0200

    Alarm bell image added

--- components/AlarmView.jsx
+++ components/AlarmView.jsx
@@ -1,11 +1,11 @@
 const AlarmView = ({ changeView }) => {
     return (
       <div className="alarm-view">
+        <img src="../img/alarm-bell.png" alt="Alarm clock" />
         <h1>Time's up!</h1>
-        <button className="start-btn" onClick={() => changeView('SetTimer')}>SET NEW TIMER</button>
+        <button className="newtimer-btn" onClick={() => changeView('SetTimer')}>SET NEW TIMER</button>
       </div>
     );
   };
   
   export default AlarmView;
-  
