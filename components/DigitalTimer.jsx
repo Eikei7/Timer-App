dedcb81cefad438e58e9f7e04053ab5e66403b6c
Erik Karlsson <erikmatfors@gmail.com>
2024-10-24 09:27:16 +0200

    Added 0 in front of seconds

--- components/DigitalTimer.jsx
+++ components/DigitalTimer.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import useTimer from 'easytimer-react-hook';
 
 const DigitalTimer = ({ startValues, changeView }) => {
-  // Användning av useTimer-hooken
   const [timer, isTargetAchieved] = useTimer({
     countdown: true,
     precision: 'seconds',
@@ -12,43 +11,45 @@ const DigitalTimer = ({ startValues, changeView }) => {
   const [timeValues, setTimeValues] = useState(timer.getTimeValues());
 
   useEffect(() => {
-    console.log('Startvärden:', startValues); // Logga startvärden för att se om de tas emot
+    console.log('Startvärden:', startValues);
 
     if (startValues && startValues.minutes > 0) {
-      console.log('Startar timern med:', startValues.minutes, 'minuter'); // Logga när timern startar
+      console.log('Startar timern med:', startValues.minutes, 'minuter');
       timer.start({
         countdown: true,
         startValues: startValues
       });
     }
 
-    // Lyssna på uppdateringar av sekunder och uppdatera state
     timer.addEventListener('secondsUpdated', () => {
-      console.log('Tid uppdaterad:', timer.getTimeValues()); // Logga uppdaterad tid
+      console.log('Tid uppdaterad:', timer.getTimeValues());
       setTimeValues(timer.getTimeValues());
     });
 
-    // Logga när målet uppnås
     timer.addEventListener('targetAchieved', () => {
       console.log('Tiden är slut!');
+      changeView('AlarmView');
     });
 
     return () => {
-      console.log('Timer stoppas'); // Logga när timern stoppas
-      timer.stop(); // Stoppa timern när komponenten avmonteras
+      console.log('Timer stoppas');
+      timer.stop();
     };
-  }, [startValues, timer]);
+  }, [startValues, timer, changeView]);
 
   // Funktion för att avbryta timern och navigera tillbaka till SetTimer
   const abortTimer = () => {
-    console.log('Timer avbruten'); // Logg för att se när timern avbryts
-    timer.stop(); // Stoppa timern när knappen klickas
-    changeView('SetTimer'); // Navigera tillbaka till SetTimer
+    console.log('Timer avbruten');
+    timer.stop();
+    changeView('SetTimer');
   };
+
+  // Lägg till nolla framför sekunderna om de är mindre än 10
+  const formattedSeconds = String(timeValues.seconds).padStart(2, '0');
 
   return (
     <div className="digital-timer">
-      <h1 className='digital-text'>{timeValues.minutes} : {timeValues.seconds}</h1>
+      <h1 className="digital-text">{timeValues.minutes} : {formattedSeconds}</h1>
       {isTargetAchieved && <p>Time's up!</p>}
       
       <button onClick={abortTimer} className="abort-btn">
